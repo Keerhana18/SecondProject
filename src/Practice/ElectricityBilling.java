f7baa04330a74cd2f72bc9526c06a20b04a401f4
ranand027 <ranand027@anandhaumarsmbp.lan>
2022-02-13 22:22:30 -0500

    init

---
+++ src/Practice/ElectricityBilling.java
@@ -0,0 +1,21 @@
+package Practice;
+
+public class ElectricityBilling {
+	int unit = 110;
+	{
+  if (unit >100) {
+	  System.out.println("Bill:"+unit*2 );
+  }
+	else if(unit>100) {
+		System.out.println(unit*3);
+	}
+		else {
+			System.out.println(unit);
+		}
+		
+	}
+		
+
+	}
+
+
