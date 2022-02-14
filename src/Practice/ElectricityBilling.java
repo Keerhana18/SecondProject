a868144d3d7acda45f544fb5321cb50b139b8c60
ranand027 <ranand027@anandhaumarsmbp.lan>
2022-02-14 13:56:50 -0500

    second commit

--- src/Practice/ElectricityBilling.java
+++ src/Practice/ElectricityBilling.java
@@ -1,21 +1,34 @@
 package Practice;
 
+import java.util.Scanner;
+
 public class ElectricityBilling {
+	
 	int unit = 110;
 	{
+	
   if (unit >100) {
 	  System.out.println("Bill:"+unit*2 );
   }
-	else if(unit>100) {
+	else if(unit<100) {
 		System.out.println(unit*3);
 	}
 		else {
 			System.out.println(unit);
 		}
-		
 	}
 		
-
+	public void waterBill() {
+		Scanner aObj = new Scanner(System.in);
+		System.out.println("Enter gallons used:");
+		int gObj = aObj.nextInt();
+        System.out.println(gObj*2);
+	}
+		
+public static void main(String[] args) {
+	ElectricityBilling eObj = new ElectricityBilling();
+	eObj.waterBill();
+}
 	}
 
 
